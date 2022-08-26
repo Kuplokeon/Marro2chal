96c9cdae4db565fdd90f5c8fb8c4b4594d14cf39
Kuplokeon <reece.gaddie@gmail.com>
2022-08-25 20:12:02 -0400

    Misc Changes
    muahahahhaha

---
+++ Marro2chal/Assets/MainMenuManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public enum MenuName
+{
+    asd,
+    gfdgdf,
+}
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMenu(int menuName)
+    {
+        //
+    }
+}
